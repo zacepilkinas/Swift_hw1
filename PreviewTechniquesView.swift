5744ecf9ebd4f3da599855c529f704ee1acf3fa9
zacepilkinas <88345619+zacepilkinas@users.noreply.github.com>
2022-01-09 19:34:13 +0300

    Add files via upload

---
+++ PreviewTechniquesView.swift
@@ -0,0 +1,21 @@
+////
+////  PreviewTechniquesView.swift
+////  SwiftHw1
+////
+////  Created by Anastasia on 06.01.2022.
+////
+//
+//import SwiftUI
+//
+//struct PreviewTechniquesView: View {
+//    var body: some View{
+//        Text("Hello, world!")
+//    }
+//}
+//
+//struct PreviewTechniquesView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PreviewTechniquesView()
+//            .previewDevice("iPhone 11")
+//    }
+//}
